929f1290c2e90660bd03f379f8a67b97ae224418
maj3r1819 <major1819@gmail.com>
2021-02-13 11:52:30 +0530

    blogs can be now added from the /admin page using models.py and migratations

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render #render used to access Templates
 from django.http import HttpResponse #handles traffic from the homepage of our blog
+from .models import Post
 
-# Create your views here.
+
+
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = { 'posts': Post.objects.all()}
+    return render(request, 'blog/home.html',context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html',{'title': 'About'})
