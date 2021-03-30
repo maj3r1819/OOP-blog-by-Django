ef2ed3fefb4f2307beb4e1d8796cea17bfec6ae9
Sagar <major1819@gmail.com>
2021-03-30 11:52:18 +0530

    OK

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render #render used to access Templates
+from django.shortcuts import render, get_object_or_404 #render used to access Templates
 from django.http import HttpResponse #handles traffic from the homepage of our blog
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import (
     ListView, DetailView, CreateView, UpdateView, DeleteView)
 
@@ -15,6 +16,20 @@ class PostListView(ListView):
     template_name = 'blog/home.html' #<app>/<model>_<viewpoint>.html
     context_object_name = 'posts'
     ordering = ['date']#changes order of post ('-date' of u want blogs in reverse order)
+    paginate_by = 5
+
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html' #<app>/<model>_<viewpoint>.html
+    context_object_name = 'posts'
+    #ordering = ['date']#changes order of post ('-date' of u want blogs in reverse order)
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username = self.kwargs.get('username'))
+        return Post.objects.filter(author = user).order_by('date')
 
 class PostDetailView(DetailView):
     model = Post
